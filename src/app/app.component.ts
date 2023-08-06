90dcf2be9794e107ac1cc600ec4d11191c2f6795
gleysi <sonia.mireya.miranda@gmail.com>
2023-08-05 21:32:58 -0600

    enter a US zip code and store an array of all entered zipcodes in local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular';
+  zipcodesArray = [];
+
+  storeZipCode(zipcode) {
+    console.log(zipcode);
+    this.zipcodesArray.push(zipcode);
+    // console.log(this.zipcodesArray);
+    localStorage.setItem('zipCodesEntered', JSON.stringify(this.zipcodesArray));
+
+    const getZipCodesStored = localStorage.getItem('zipCodesEntered');
+    console.log('getZipCodesStored', getZipCodesStored);
+  }
 }
