0279ac4d9c812f7913af1581a8b0ee50cbaad176
gleysi <sonia.mireya.miranda@gmail.com>
2023-08-05 23:25:46 -0600

    get API data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -8,14 +10,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Angular';
   zipcodesArray = [];
+  apiKey = '5a4b2d457ecbef9eb2a71e480b947604';
+
+  private url =
+    'https://api.openweathermap.org/data/2.5/weather?zip=10001&units=imperial&APPID=' +
+    this.apiKey;
+
+  constructor(private http: HttpClient) {}
 
   storeZipCode(zipcode) {
     console.log(zipcode);
     this.zipcodesArray.push(zipcode);
-    // console.log(this.zipcodesArray);
     localStorage.setItem('zipCodesEntered', JSON.stringify(this.zipcodesArray));
 
     const getZipCodesStored = localStorage.getItem('zipCodesEntered');
     console.log('getZipCodesStored', getZipCodesStored);
+
+    this.getForecast().subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  // TODO: Change this to a service
+  getForecast(): Observable<any> {
+    return this.http.get<any>(this.url);
   }
 }
