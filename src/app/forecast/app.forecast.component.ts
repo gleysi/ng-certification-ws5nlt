7f1bfe9f17c6660f079ef8d20dc5afed74d54b05
gleysi <sonia.mireya.miranda@gmail.com>
2023-08-20 21:50:09 -0600

    Get Next Days Observable

--- src/app/forecast/app.forecast.component.ts
+++ src/app/forecast/app.forecast.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './app.forecast.component.html',
   styleUrls: ['./app.forecast.css'],
 })
-export class ForecastComponent {}
+export class ForecastComponent {
+  constructor(
+    weatherService: WeatherService
+  ) {
+    weatherService.getNextDays('New York').subscribe(res => {
+      console.log(res);
+    });
+  }
+}
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,31 +6,42 @@ import { Observable } from 'rxjs';
 export class WeatherService {
   apiKey = '5a4b2d457ecbef9eb2a71e480b947604';
 
-  private url =
-    'https://api.openweathermap.org/data/2.5/weather?zip=10001&units=imperial&APPID=' +
-    this.apiKey;
+  private url = 'https://api.openweathermap.org/data/2.5/';
 
-  constructor(
-    private http: HttpClient
-  ) {
+  constructor(private http: HttpClient) {}
+
+  getNextDays(city: string): Observable<any> {
+    return this.http.get<any>(
+      this.url + 'forecast/daily?q=' + city + '&APPID=' + this.apiKey
+    );
   }
 
   // Observable
   getForecast(zipCode: string): Observable<any> {
-    return this.http.get<any>('https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&units=imperial&APPID=' +
-    this.apiKey);
+    return this.http.get<any>(
+      this.url +
+        'weather?zip=' +
+        zipCode +
+        '&units=imperial&APPID=' +
+        this.apiKey
+    );
   }
 
   // Promise
   async getWeather(zipCode: string): Promise<any> {
     try {
-      const response = await this.http.get('https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + '&units=imperial&APPID=' +
-      this.apiKey, {});
-  
+      const response = await this.http.get(
+        this.url +
+          'weather?zip=' +
+          zipCode +
+          '&units=imperial&APPID=' +
+          this.apiKey,
+        {}
+      );
+
       return Promise.resolve(response);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
-    
   }
 }
